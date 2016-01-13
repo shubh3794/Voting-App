ebec0009ae0ed227d3b94fa7153e15944d216ad6
shubham <shubh.aggarwal.37@gmail.com>
2016-01-13 17:28:46 +0530

    Everything works now

--- votingapp/voteApp/authentication/views.py
+++ votingapp/voteApp/authentication/views.py
@@ -12,19 +12,22 @@ def user_signup(request):
 		email = request.POST['email']
 		password = request.POST['password']
 		username = request.POST['username']
-		acc = authenticate(email=email,password=password)
-		if not acc:
-			user = Account.objects.create_user(email,password,username=username)
-			user = authenticate(email=email,password=password)
-			login(request,user)
-			return HttpResponseRedirect(reverse('polls:index'))
-		else:
-			
-			if acc.is_active:
-				login(request,acc)
+		a = Account.objects.filter(email=email)
+		if len(a)==0 and len(username.replace(" ",""))>0:
+			acc = authenticate(email=email,password=password)
+			if not acc:
+				user = Account.objects.create_user(email,password,username=username)
+				user = authenticate(email=email,password=password)
+				login(request,user)
 				return HttpResponseRedirect(reverse('polls:index'))
 			else:
-				return render(request,'polls/signup.html',{'error':'User already exists, login with correct details'})
+				if acc.is_active:
+					login(request,acc)
+					return HttpResponseRedirect(reverse('polls:index'))
+				else:
+					return render(request,'polls/signup.html',{'error':'User already exists, login with correct details'})
+		else:
+			return render(request,'polls/signup.html',{'error_message':'This email is already registered or you may have entered invalid username, try again'})
 
 	else:
 		return render(request,'polls/signup.html',{})
